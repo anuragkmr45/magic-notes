b94e13ac4d018d9b7c4a65b1890d9faf4ef383ec
apikmeister <afiq.mohamad90@gmail.com>
2023-10-09 14:42:28 +0800

    fix:#6 Fix listing empty notes - Add toast to notify user that the notes is empty.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,24 @@
- 
 console.log("Welcome to notes app. This is app.js");
 showNotes();
+
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e) {
-  
-let addTxt = document.getElementById("addTxt");
- 
-let notes = localStorage.getItem("notes");
+addBtn.addEventListener("click", function (e) {
+  console.log("fak");
+  e.preventDefault();
+
+  let addTxt = document.getElementById("addTxt");
+
+  if (!addTxt.value.trim()) {
+    const toast = document.getElementById("emptyFormToast");
+    toast.style.display = "block";
+    setTimeout(() => {
+      toast.style.display = "none";
+    }, 3000);
+    return;
+  }
+
+  let notes = localStorage.getItem("notes");
 
   if (notes == null) {
     notesObj = [];
@@ -17,7 +28,7 @@ let notes = localStorage.getItem("notes");
   notesObj.push(addTxt.value);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
-//   console.log(notesObj);
+  //   console.log(notesObj);
   showNotes();
 });
 
@@ -30,7 +41,7 @@ function showNotes() {
     notesObj = JSON.parse(notes);
   }
   let html = "";
-  notesObj.forEach(function(element, index) {
+  notesObj.forEach(function (element, index) {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
@@ -51,7 +62,7 @@ function showNotes() {
 
 // Function to delete a note
 function deleteNote(index) {
-//   console.log("I am deleting", index);
+  //   console.log("I am deleting", index);
 
   let notes = localStorage.getItem("notes");
   if (notes == null) {
@@ -65,24 +76,21 @@ function deleteNote(index) {
   showNotes();
 }
 
-
-let search = document.getElementById('searchTxt');
-search.addEventListener("input", function(){
-
-    let inputVal = search.value.toLowerCase();
-    // console.log('Input event fired!', inputVal);
-    let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
-        let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
-            element.style.display = "block";
-        }
-        else{
-            element.style.display = "none";
-        }
-        // console.log(cardTxt);
-    })
-})
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  // console.log('Input event fired!', inputVal);
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+    // console.log(cardTxt);
+  });
+});
 
 /*
 Further Features:
@@ -90,4 +98,4 @@ Further Features:
 2. Mark a note as Important
 3. Separate notes by user
 4. Sync and host to web server 
-*/ 
+*/
